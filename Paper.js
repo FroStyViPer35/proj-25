27003b485934f22a3e8f957a79508c43c1444f00
FroStyViPer35 <83050310+FroStyViPer35@users.noreply.github.com>
2021-09-02 23:17:37 -0700

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,17 @@
+class Paper
+{
+    constructor(x,y)
+	{
+        var options={
+            isStatic:false,
+            restitution: 0.3,
+            friction:0,
+            density:1.2
+
+            
+
+        }
+        image (this.image,0, 0,this.width, this.height)
+    }
+
+}
